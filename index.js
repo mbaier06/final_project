864015e0b0bdd90b7fdfd87993ee3fb3eb0a6550
Matt Baier <mbaier06@gmail.com>
2021-12-17 14:52:38 -0600

    Added code to consume Twilio library & use env var

--- index.js
+++ index.js
@@ -51,6 +51,12 @@ firebase.auth().onAuthStateChanged(async function(user) {
     alert("Location has been submitted!"); 
 
     //Twilio SMS API Code to send message to cart operator
+    require('dotenv').config();
+    console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', process.env.TWILIO_ACCOUNT_SID);
+
+    require('dotenv').config();
+    console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', process.env.TWILIO_AUTH_TOKEN);
+    
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
