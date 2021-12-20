59c5fd1e5a275dced689f90842d64696dd63cc19
Matt Baier <mbaier06@gmail.com>
2021-12-20 16:58:18 -0600

    tinkering with "require" command

--- index.js
+++ index.js
@@ -63,6 +63,18 @@ firebase.auth().onAuthStateChanged(async function(user) {
     console.log(requestCourse)
     console.log(requestHole)
 
+    const accountSid = 'AC09fd9693c3ea973c26dddf0c1f313c0f';
+    const authToken = 'b3d92ffa79994a2b84720bd31ed7bd2';
+    console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', accountSid)
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+    .create({
+      body: 'A golfer has submitted an order',
+      to: '+12033139748',
+      from: '+18648637454',
+    })
+    .then((message) => console.log(message.sid));
   })
   
   } else {
--- netlify/functions/sms.js
+++ netlify/functions/sms.js
@@ -3,15 +3,16 @@
 //Twilio Credentials
 //require('dotenv').config();
 //const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const accountSid = AC09fd9693c3ea973c26dddf0c1f313c0f;
-const authToken = b3d92ffa79994a2b84720bd31ed7bd2;
-console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', TWILIO_ACCOUNT_SID)
-const client = require('twilio')(accountSid, authToken);
+const accountSid = 'AC09fd9693c3ea973c26dddf0c1f313c0f';
+const authToken = 'b3d92ffa79994a2b84720bd31ed7bd2';
+console.log('Your environment variable TWILIO_ACCOUNT_SID has the value: ', accountSid)
+const twilio = require('twilio'); 
+const client = new twilio(accountSid, authToken);
 
 client.messages
 .create({
    body: 'A golfer has submitted an order',
+   to: '+12033139748',
    from: '+18648637454',
-   to: '+12033139748'
  })
-.then(message => console.log(message.sid));
+.then((message) => console.log(message.sid));
